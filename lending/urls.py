79c8d022246b6ad78f5f954c8d8e8ce959a5d916
Candido Armando <candido@gmail.com>
2018-10-12 08:17:09 -0400

    creando la url y la plantilla para crear un nuevo libro

--- lending/urls.py
+++ lending/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('list/', views.BookListView.as_view(), name='book-list'),
     path('student/list/', views.StudentListView.as_view(), name='student-query'),
     path('student/<int:pk>/detail/', views.StudentDetailView.as_view(), name='student-detail'),
+
+    path('book/create/', views.BookCreateView.as_view(), name='book-create'),
 ]
