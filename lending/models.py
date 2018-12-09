7e91cea3c0d98d3f38f3a9be5aba28e593ac0c7a
Candido Armando <candido@gmail.com>
2018-12-08 22:55:33 -0500

    mostrando los formularios por defecto en español

--- lending/models.py
+++ lending/models.py
@@ -8,9 +8,9 @@ class CustomModel(models.Model):
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=100)
-    number = models.PositiveIntegerField(default=0)
-    cant = models.PositiveIntegerField(default=0)
+    name = models.CharField('Nombre', max_length=100)
+    number = models.PositiveIntegerField('Número', default=0)
+    cant = models.PositiveIntegerField('Cantidad', default=0)
 
     def to_dict(self):
         return {
@@ -29,11 +29,11 @@ class Book(models.Model):
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=80)
-    last_name = models.CharField(max_length=150)
-    ci = models.CharField(max_length=11)
-    number = models.PositiveIntegerField(default=0)
-    academic_year = models.PositiveIntegerField(default=1)
+    first_name = models.CharField('Nombre', max_length=80)
+    last_name = models.CharField('Apellidos', max_length=150)
+    ci = models.CharField('CI', max_length=11)
+    number = models.PositiveIntegerField('Número', default=0)
+    academic_year = models.PositiveIntegerField('Año académico', default=1)
     books = models.ManyToManyField(
         Book,
         through='Lending',
@@ -72,7 +72,7 @@ class Student(models.Model):
 class Lending(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    date = models.DateField(auto_now=True)
+    date = models.DateField('Fecha', auto_now=True)
 
     def __str__(self):
         return '{} --> {}'.format(self.student, self.book)
