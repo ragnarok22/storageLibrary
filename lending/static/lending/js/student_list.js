f0e3f11f841e86dbad38536a571ac2b70d200774
Candido Armando <candido@gmail.com>
2018-12-26 10:28:11 -0500

    añadiendo libros prestados a un estudiante y actualizando los datos por ajax

--- lending/static/lending/js/student_list.js
+++ lending/static/lending/js/student_list.js
@@ -6,6 +6,8 @@ $(function () {
         $.ajax(href, {
             method: 'GET',
             success: function (data) {
+                const add_lending = $('#add-lending');
+                const update_student_form = $('#update-student-form');
                 $('#number').html('N&uacute;mero: ' + data['number']);
                 $('#student-number').prop('value', data['number']);
                 $('#student-number-label').prop('class', 'active');
@@ -21,9 +23,10 @@ $(function () {
                 $('#last-name').html('Apellidos: ' + data['last_name']);
                 $('#student-lastname').prop('value', data['last_name']);
                 $('#student-lastname-label').prop('class', 'active');
-                $('#update-student-form').prop('action', data['update_url']);
+                update_student_form.prop('action', data['update_url']);
                 $('#delete').prop('href', data['delete_url']);
                 $('#update').prop('href', data['update_url']);
+                add_lending.attr('data-student-pk', data['pk']);
                 $('#student-info').attr('style', 'display: flex');
                 let lending_html = '';
                 if (data['lending'].length !== 0) {
@@ -38,6 +41,30 @@ $(function () {
                     }
                     lending_html += '</ol>';
                     $('#lending').html(lending_html);
+
+                    function remove_lending(url) {
+                        $.ajax(url, {
+                            method: 'POST',
+                            success: function (response, status) {
+                                swal(
+                                    'Hecho',
+                                    'Se ha entregado el libro',
+                                    status
+                                );
+                                //reload the student detail by ajax request
+                                get_student_detail(href);
+
+                            },
+                            error: function (response, status) {
+                                swal(
+                                    'Error',
+                                    'No se ha podido ejecutar la acción',
+                                    status
+                                );
+                            },
+                        });
+                    }
+
                     $('a[data-type="remove-lending"]').click(function (event) {
                         event.preventDefault();
                         swal({
@@ -49,32 +76,113 @@ $(function () {
                             cancelButtonText: 'No',
                         }).then((result) => {
                             if (result.value) {
-                                $.ajax($(this).prop('href'), {
-                                    method: 'POST',
-                                    success: function (response, status) {
-                                        swal(
-                                            'Hecho',
-                                            'Se ha entregado el libro',
-                                            status
-                                        );
-                                        //reload the student detail by ajax request
-                                        get_student_detail(href);
-
-                                    },
-                                    error: function (response, status) {
-                                        swal(
-                                            'Error',
-                                            'No se ha podido ejecutar la acción',
-                                            status
-                                        );
-                                    },
-                                });
+                                remove_lending($(this).prop('href'));
                             }
                         });
                     });
                 } else {
                     $('#lending').html(lending_html);
                 }
+
+                function create_lending(url, student, book) {
+                    $.ajax(url, {
+                        method: 'POST',
+                        data: {
+                            'student': student,
+                            'book': book,
+                        },
+                        success: function () {
+                            swal({
+                                position: 'top-end',
+                                type: 'success',
+                                title: 'Libro añadido',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                            get_student_detail(href);
+                        },
+                        error: function (response, status) {
+                            swal({
+                                title: 'Error ' + response.status,
+                                html: 'Ha ocurrido un error. ' + response.statusText + '. ' + response.responseText,
+                                type: status
+                            });
+                        }
+                    });
+                }
+
+                function get_book_list() {
+                    $.ajax($('tbody#book-list').attr('data-href'), {
+                        method: 'GET',
+                        success: function (response) {
+                            let result_html = '';
+                            const book_list = response['book_list'];
+                            for (let i = 0; i < book_list.length; i++) {
+                                const book = book_list[i];
+                                result_html += '<tr><td>' + book['name'] + '</td><td>' + book['editorial'] + '</td><td>' +
+                                    book['publish_year'] + '</td><td><a href="#" data-book-pk="' + book['pk'] +
+                                    '" data-type="lending"><i class="fa fa-plus"></i></a></td></tr>';
+                            }
+                            $('#book-list').html(result_html);
+                            const lending_plus = $('a[data-type="lending"]');
+                            lending_plus.click(function (event) {
+                                event.preventDefault();
+                                const lending = $('#add-lending');
+                                create_lending(lending.attr('data-create-lending-url'), lending.attr('data-student-pk'), $(this).attr('data-book-pk'));
+                            });
+                        },
+                        error: function (response, status) {
+                            swal({
+                                title: 'Error',
+                                html: 'Ha ocurrido un error',
+                                type: status
+                            });
+                        }
+                    });
+                }
+
+                function update_student(url) {
+                    $.ajax(url, {
+                        method: 'POST',
+                        data: {
+                            'first_name': $('#student-firstname').val(),
+                            'last_name': $('#student-lastname').val(),
+                            'ci': $('#student-ci').val(),
+                            'number': $('#student-number').val(),
+                            'academic_year': $('#student-academic-year').val(),
+                        },
+                        success: function (response, status) {
+                            $('#update-student').modal('hide');
+                            swal({
+                                type: status,
+                                title: 'Guardado',
+                                text: 'Se ha guardado éxitosamente',
+                            }).then((result) => {
+                                if (result.value) {
+                                    get_student_detail(href);
+                                    get_student_list();
+                                }
+                            });
+                        },
+                        error: function (response, status) {
+                            swal({
+                                type: status,
+                                title: 'Error',
+                                text: 'No se ha podido guardar',
+                            });
+                        },
+                    });
+                }
+
+                update_student_form.submit(function (event) {
+                    event.preventDefault();
+                    update_student($(this).prop('action'));
+                });
+
+                add_lending.click(function (event) {
+                    event.preventDefault();
+                    get_book_list();
+                });
             },
             error: function (response, status) {
                 swal({
@@ -116,10 +224,6 @@ $(function () {
                     event.preventDefault();
                     delete_student($(this).prop('href'));
                 });
-                $('#update-student-form').submit(function (event) {
-                    event.preventDefault();
-                    update_student($(this).prop('action'));
-                });
             },
             error: function (response, status) {
                 swal({
@@ -155,7 +259,6 @@ $(function () {
                         });
                     },
                     error: function (response, status) {
-                        console.log(response);
                         swal(
                             'Error',
                             'No se ha podido ejecutar',
@@ -167,71 +270,11 @@ $(function () {
         });
     }
 
-    function update_student(url) {
-        $.ajax(url, {
-            method: 'POST',
-            data: {
-                'first_name': $('#student-firstname').val(),
-                'last_name': $('#student-lastname').val(),
-                'ci': $('#student-ci').val(),
-                'number': $('#student-number').val(),
-                'academic_year': $('#student-academic-year').val(),
-            },
-            success: function (response, status) {
-                $('#update-student').modal('hide');
-                swal({
-                    type: status,
-                    title: 'Guardado',
-                    text: 'Se ha guardado éxitosamente',
-                }).then((result) => {
-                    if (result.value) {
-                        //get_student_detail(url);
-                    }
-                });
-            },
-            error: function (response, status) {
-                swal({
-                    type: status,
-                    title: 'Error',
-                    text: 'No se ha podido guardar',
-                });
-            },
-        });
-    }
-
     $('form[method="get"]').submit(function (event) {
         event.preventDefault();
         get_student_list();
     });
 
     get_student_list();
-
-    function get_book_list() {
-        $.ajax($('tbody#book-list').attr('data-href'), {
-            method: 'GET',
-            success: function (response) {
-                let result_html = '';
-                const book_list = response['book_list']
-                for (let i = 0; i < book_list.length; i++) {
-                    const book = book_list[i];
-                    result_html += '<tr><td>' + book['name'] + '</td><td>' + book['editorial'] +
-                        '</td><td>' + book['publish_year'] + '</td><td><a href="#"><i class="fa fa-plus"></i></a></td></tr>';
-                }
-                $('#book-list').html(result_html);
-            },
-            error: function (response, status) {
-                swal({
-                    title: 'Error',
-                    html: 'Ha ocurrido un error',
-                    type: status
-                });
-            }
-        });
-    }
-
-    $('#add-lending').click(function (event) {
-        event.preventDefault();
-        get_book_list();
-    });
 
 });
